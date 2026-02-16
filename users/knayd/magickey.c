52f49e5bd3cb698484176dcfe03a80da9415b89c
Deryan Argueta <deryan.hndz@gmail.com>
2026-02-15 21:07:11 -0600

    fix: set magic key/mods independently so it correctly fallbacks to default values

--- users/knayd/magickey.c
+++ users/knayd/magickey.c
@@ -1,37 +1,39 @@
 #include "magickey.h"
 #include "os_mode.h"
 
-bool handle_magic_key(
-    magic_key_config_t config,
-    uint16_t keycode,
-    keyrecord_t *record
-) {
+bool handle_magic_key(magic_key_config_t config, uint16_t keycode, keyrecord_t *record) {
     if (keycode != config.trigger) return true;
 
     uint16_t key;
-    uint8_t mods;
+    uint8_t  mods;
 
-    // Select key and mods based on OS
+    // Determine key
     if (is_linux_os() && config.linux_key) {
         key = config.linux_key;
-        mods = config.linux_mods;
     } else if (is_mac_os() && config.mac_os_key) {
         key = config.mac_os_key;
-        mods = config.mac_os_mods;
     } else {
         key = config.default_key;
+    }
+
+    // Determine mods (independent from key)
+    if (is_linux_os() && config.linux_mods) {
+        mods = config.linux_mods;
+    } else if (is_mac_os() && config.mac_os_mods) {
+        mods = config.mac_os_mods;
+    } else {
         mods = config.default_mods;
     }
 
     if (record->event.pressed) {
-        if(mods) {
+        if (mods) {
             add_mods(mods);
             send_keyboard_report();
         }
         register_code(key);
     } else {
         unregister_code(key);
-        if(mods) {
+        if (mods) {
             del_mods(mods);
             send_keyboard_report();
         }
